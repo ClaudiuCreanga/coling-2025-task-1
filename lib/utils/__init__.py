bdd1ba2462736275dc2dcd4a7afcfd737f548535
Teodor George MARCHITAN <tmarchitan@bitdefender.com>
2024-09-28 07:25:55 +0300

    Add set_seed method in utils module

--- lib/utils/__init__.py
+++ lib/utils/__init__.py
@@ -1,6 +1,17 @@
 import torch
+import random
+import numpy as np
 from datetime import datetime
 from lib.utils.constants import DATETIME_FORMAT
+
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 def elapsed_time(start_time: float, end_time: float) -> (float, str):
